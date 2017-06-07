3a24631f73d5be3c01b94c12ec8210980aacb40c
Sharon Gao <isotera@gmail.com>
2017-06-07 17:16:51 -0400

    V 1.2 - Bug fixes, color picker, open files

--- PyPaint.py
+++ PyPaint.py
@@ -2,7 +2,9 @@
 # Sharon Gao, 2017
 from tkinter import filedialog
 from tkinter import *
+from tkinter.colorchooser import *
 import pyscreenshot as ImageGrab
+from PIL import Image
 
 # TOOLS
 PENCIL, BRUSH, ERASER, LINE, RECTANGLE, OVAL = list(range(6))
@@ -16,32 +18,32 @@ class Paint:
         self.canvas.bind('<B1-Motion>', self.draw)
 
     def draw(self, event):
-        if self._tool is None or self._obj is None:
+        if self._tool is None:
             return
         x, y = self.lastx, self.lasty
         if self._tool in (LINE, RECTANGLE, OVAL):
             self.canvas.coords(self._obj, (x, y, event.x, event.y))
         elif self._tool in (PENCIL, BRUSH, ERASER):
-            if self.lastx is not None and self.lasty is not None:
-                if self._tool == PENCIL:
-                    self.canvas.create_line(self.lastx, self.lasty, event.x, event.y, fill = self._color)
-                elif self._tool == BRUSH:
-                    if self._width is None:
-                        x1, y1 = (event.x - 5), (event.y - 5)
-                        x2, y2 = (event.x + 5), (event.y + 5)
-                    else:
-                        x1, y1 = (event.x - self._width), (event.y - self._width)
-                        x2, y2 = (event.x + self._width), (event.y + self._width)
-                    self.canvas.create_rectangle(x1, y1, x2, y2, fill = self._color, outline = self._color)
-                elif self._tool == ERASER:
-                    if self._width is None:
-                        x1, y1 = (event.x - 15), (event.y - 15)
-                        x2, y2 = (event.x + 15), (event.y + 15)
-                    else:
-                        x1, y1 = (event.x - self._width), (event.y - self._width)
-                        x2, y2 = (event.x + self._width), (event.y + self._width)
-                    self.canvas.create_rectangle(x1, y1, x2, y2, fill = "#ffffff", outline = "#ffffff")
-                self.lastx, self.lasty = event.x, event.y
+            #if self.lastx is not None and self.lasty is not None:
+            if self._tool == PENCIL:
+                self.canvas.create_line(self.lastx, self.lasty, event.x, event.y, fill = self._color)
+            elif self._tool == BRUSH:
+                if self._width is None:
+                    x1, y1 = (event.x - 5), (event.y - 5)
+                    x2, y2 = (event.x + 5), (event.y + 5)
+                else:
+                    x1, y1 = (event.x - self._width), (event.y - self._width)
+                    x2, y2 = (event.x + self._width), (event.y + self._width)
+                self.canvas.create_rectangle(x1, y1, x2, y2, fill = self._color, outline = self._color)
+            elif self._tool == ERASER:
+                if self._width is None:
+                    x1, y1 = (event.x - 15), (event.y - 15)
+                    x2, y2 = (event.x + 15), (event.y + 15)
+                else:
+                    x1, y1 = (event.x - self._width), (event.y - self._width)
+                    x2, y2 = (event.x + self._width), (event.y + self._width)
+                self.canvas.create_rectangle(x1, y1, x2, y2, fill = "#ffffff", outline = "#ffffff")
+            self.lastx, self.lasty = event.x, event.y
                 
     # Updates x and y coordinates
     # Anchors starting coordinates for shapes
@@ -84,6 +86,13 @@ class Paint:
 
 class Tool:
     def __init__(self, whiteboard, parent=None):
+        self.file_to_open = None
+        self.custom_color = None
+        self._curr_tool = None
+        self._curr_color = None
+        self._curr_width = None
+        self._curr_fill = None
+        
         # TOOL ICONS
         self.pencil = PhotoImage(file = "Images/pencil_tool.gif")
         self.brush = PhotoImage(file = "Images/brush_tool.gif")
@@ -103,6 +112,8 @@ class Tool:
         self.blue = PhotoImage(file = "Images/blue.gif") #0000ff
         self.magenta = PhotoImage(file = "Images/magenta.gif") #ff00ff
         self.brown = PhotoImage(file = "Images/brown.gif") #883d00
+        self.colorwheel = PhotoImage(file = "Images/colorwheel.gif")
+        self.pick_custom = PhotoImage(file = "Images/custom.gif")
         
         # BRUSH WIDTH ICONS
         self.one = PhotoImage(file = "Images/1.gif")
@@ -131,16 +142,16 @@ class Tool:
         ]
         
         COLORS = [
-            (self.black, '#000000'),
-            (self.gray, '#808080'),
-            (self.white, '#FFFFFF'),
-            (self.red, '#FF0000'),
-            (self.yellow, '#FFFF00'),
-            (self.green, '#00FF00'),
-            (self.cyan, '#00FFFF'),
-            (self.blue, '#0000FF'),
-            (self.magenta, '#FF00FF'),
-            (self.brown, '#883d00')
+            (self.black, '#000000', 2),
+            (self.gray, '#808080', 2),
+            (self.white, '#FFFFFF', 2),
+            (self.red, '#FF0000', 1),
+            (self.yellow, '#FFFF00', 1),
+            (self.green, '#00FF00', 1),
+            (self.cyan, '#00FFFF', 1),
+            (self.blue, '#0000FF', 1),
+            (self.magenta, '#FF00FF', 2),
+            (self.brown, '#883d00', 2)
         ]
         
         WIDTH = [
@@ -158,65 +169,98 @@ class Tool:
         ]
         
         self.whiteboard = whiteboard
-        frame1 = Frame(parent)
-        frame2 = Frame(parent)
-        self._curr_tool = None
-        self._curr_color = None
-        self._curr_width = None
-        self._curr_fill = None
+        frame1 = Frame(parent, width = 40)
+        frame2 = Frame(parent, width = 40)
+        frame1.pack_propagate(False) # do not shrink/expand based on label size
+        frame2.pack_propagate(False) # do not shrink/expand based on label size
         
-        # Create and place icons
+        
+#------------------ ICON CREATION AND PLACEMENT ------------------------
+        # TOOLS - FRAME 1
         for img, name in TOOLS:
             lbl = Label(frame1, relief='raised', image = img)
             lbl._tool = name
             lbl.bind('<Button-1>', self.update_tool)
             lbl.pack(padx = 6, pady = 3)
-        spacer = Label(frame1, image = self.white)
-        spacer.pack(padx = 6, pady = 3)
-        for img, name in COLORS:
-            lbl = Label(frame1, relief='raised', image = img)
-            lbl._color = name
-            lbl.bind('<Button-1>', self.update_color)
-            lbl.pack(padx = 6, pady = 3)
-        spacer = Label(frame1, image = self.white)
-        spacer.pack(padx = 6, pady = 3)
-        lbl = Label(frame1, relief = 'raised', image = self.save)
-        #lbl.bind('<Button-1>', self.save_image)
-        lbl.pack(padx = 6, pady = 3)
-        frame1.pack(side = 'left', fill = 'y', expand = True, pady = 6)
-        
+
+        # LINE WIDTHS - FRAME 2
         for img, value in WIDTH:
             lbl = Label(frame2, relief='raised', image = img)
             lbl._width = value
             lbl.bind('<Button-1>', self.update_width)
             lbl.pack(padx = 6, pady = 3)
+   
+        # STROKE - FRAME 1
+        lbl = Label(frame1, relief = 'raised', image = self.stroke)
+        lbl._fill = False
+        lbl.bind('<Button-1>', self.update_fill)
+        lbl.pack(padx = 6, pady = 3)
+        spacer = Label(frame1, image = self.white)
+        spacer.pack(padx = 6, pady = 3)
+        
+        # FILL - FRAME 2
+        lbl = Label(frame2, relief = 'raised', image = self.fill)
+        lbl._fill = True
+        lbl.bind('<Button-1>', self.update_fill)
+        lbl.pack(padx = 6, pady = 3)
         spacer = Label(frame2, image = self.white)
         spacer.pack(padx = 6, pady = 3)
-        for img, value in FILL:
-            lbl = Label(frame2, relief='raised', image = img)
-            lbl._fill = value
-            lbl.bind('<Button-1>', self.update_fill)
+        
+        # COLOR WHEEL - FRAME 1
+        lbl = Label(frame1, relief = 'raised', image = self.colorwheel)
+        lbl.bind('<Button-1>', self.pick_color)
+        lbl.pack(padx = 6, pady = 3)
+        
+        # CUSTOM COLOR - FRAME 2
+        # frame to contain and specify label size
+        color_frame = Frame(frame2, height = 28, width = 37)
+        color_frame.pack_propagate(0)
+        color_frame.pack(padx = 6, pady = 3)
+        self.custom = Label(color_frame, relief = 'raised', background = self.custom_color)
+        if self.custom_color is None:
+            self.custom.configure(image = self.pick_custom)
+        self.custom.pack(fill = BOTH, expand = 1)
+        
+        # COLORS - FRAME 1 AND 2
+        for img, name, num in COLORS:
+            if num == 1:
+                lbl = Label(frame1, relief = 'raised', image = img)
+            elif num == 2:
+                lbl = Label(frame2, relief = 'raised', image = img)
+            lbl._color = name
+            lbl.bind('<Button-1>', self.update_color)
             lbl.pack(padx = 6, pady = 3)
-        spacer = Label(frame2, image = self.white)
+            
+        spacer = Label(frame1, image = self.white)
         spacer.pack(padx = 6, pady = 3)
-        spacer = Label(frame2, image = self.white)
+        spacer = Label(frame1, image = self.white)
         spacer.pack(padx = 6, pady = 3)
-        spacer = Label(frame2, image = self.white)
-        spacer.pack(padx = 6, pady = 3)
-        spacer = Label(frame2, image = self.white)
-        spacer.pack(padx = 6, pady = 3)
-        spacer = Label(frame2, image = self.white)
-        spacer.pack(padx = 6, pady = 3)
-        lbl = Label(frame2, relief = 'raised', image = self.clear)
+        
+        # SAVE - FRAME 1
+        lbl = Label(frame1, relief = 'raised', image = self.save)
+        lbl.bind('<Button-1>', self.save_file)
+        lbl.pack(padx = 6, pady = 3)
+        # CLEAR - FRAME 1
+        lbl = Label(frame1, relief = 'raised', image = self.clear)
         lbl.bind('<Button-1>', self.clear_canvas)
         lbl.pack(padx = 6, pady = 3)
+
+        # END OF FRAME 1
+        frame1.pack(side = 'left', fill = 'y', expand = True, pady = 6)
+
+        spacer = Label(frame2, image = self.white)
+        spacer.pack(padx = 6, pady = 3)
+        spacer = Label(frame2, image = self.white)
+        spacer.pack(padx = 6, pady = 3)
+        
+        # OPEN - FRAME 2
         lbl = Label(frame2, relief = 'raised', image = self.open)
         lbl.bind('<Button-1>', self.open_file)
         lbl.pack(padx = 6, pady = 3)
-        lbl = Label(frame2, relief = 'raised', image = self.save)
-        lbl.bind('<Button-1>', self.save_file)
-        lbl.pack(padx = 6, pady = 3)
+        
+        # END OF FRAME 2
         frame2.pack(side='left', fill = 'y', expand = True, pady = 6)
+#----------------------------------------------------------------------
         
     # Update current value and depress selected icon
     def update_tool(self, event):
@@ -251,58 +295,72 @@ class Tool:
         self._curr_fill = lbl
         self.whiteboard.select_fill(lbl._fill)
     
+    # Opens color picker and selects custom color
+    def pick_color(self, event):
+        color = askcolor()
+        self.whiteboard.select_color(color[1])
+        self.custom.configure(background = color[1], relief = 'sunken', image = "")
+        self.custom._color = color[1]
+        self.custom.bind('<Button-1>', self.update_color)
+        self._curr_color = self.custom
+    
     def save_file(self, event):
         filename = filedialog.asksaveasfilename(initialdir = "/",title = "Select file",filetypes = (("jpeg files","*.jpg"),("png files","*.png"),("gif files","*.gif"),("bmp files","*.bmp")))
         x = root.winfo_x()
         y = root.winfo_y()
-        im = ImageGrab.grab(bbox = (x + 91, y + 31, x + 891, y + 653))
+        im = ImageGrab.grab(bbox = (x + 91, y + 31, x + 891, y + 653)) # Screenshot canvas area
         if filename is None: # on cancel, don't save
             return
         im.save(filename)
-        im.show()
+        #im.show() 
         
     def open_file(self, event):
         filename = filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("jpeg files","*.jpg"),("png files","*.png"),("gif files","*.gif"),("bmp files","*.bmp")))
-        self.file_to_open = PhotoImage(file = filename)
-        canvas.create_image(0, 0, image = self.file_to_open)
+        imgtemp = Image.open(filename)
+        if imgtemp.size[0] > 800 or imgtemp.size[1] > 600: # if image is larger than 800x600, resize
+            imgtemp = imgtemp.resize((800, 600), Image.ANTIALIAS)
+        imgtemp.save("Temp.gif", "gif")
+        self.file_to_open = PhotoImage(file = "Temp.gif") # reference to image, otherwise will be lost to garbage collection
+        canvas.delete("all") # clear canvas beforehand
+        canvas.create_image(3, 3, image = self.file_to_open, anchor = NW) # image must be anchored to be centered on screen
         
-    def popup(self, event):
-        w = PopupWindowSave(root)
-        root.wait_window(w.top)
-        x = root.winfo_x()
-        y = root.winfo_y()
-        im = ImageGrab.grab(bbox = (x + 91, y + 31, x + 891, y + 653))
-        if w.name is None:
-            return
-        file_name = w.name + w.type
-        im.save(file_name)
-        im.show()
+#    def popup(self, event):
+#        w = PopupWindowSave(root)
+#        root.wait_window(w.top)
+#        x = root.winfo_x()
+#        y = root.winfo_y()
+#        im = ImageGrab.grab(bbox = (x + 91, y + 31, x + 891, y + 653))
+#        if w.name is None:
+#            return
+#        file_name = w.name + w.type
+#        im.save(file_name)
+#        im.show()
         
     # Clear canvas
     def clear_canvas(self, event):
         canvas.delete("all")
 
 # Class for popup window to save image
-class PopupWindowSave:
-    def __init__(self, parent):
-        top = self.top = Toplevel(parent)
-        Label(top, text="File Name: ").grid(row = 1, padx = 10, pady = 5)
-        self.e = Entry(top)
-        self.e.grid(row = 1, column = 2, padx = 10, pady = 5)
-        b = Button(top, text="Save Image", command=self.ok)
-        b.grid(row = 2, column = 2, pady = 5)
+#class PopupWindowSave:
+#    def __init__(self, parent):
+#        top = self.top = Toplevel(parent)
+#        Label(top, text="File Name: ").grid(row = 1, padx = 10, pady = 5)
+#        self.e = Entry(top)
+#        self.e.grid(row = 1, column = 2, padx = 10, pady = 5)
+#        b = Button(top, text="Save Image", command=self.ok)
+#        b.grid(row = 2, column = 2, pady = 5)
         
-        self.file_type = StringVar(top)
-        self.file_type.set(".png")
-        self.option = OptionMenu(top, self.file_type, ".png", ".jpeg", ".bmp")
-        self.option.grid(row = 1, column = 3, padx = 10, pady = 5)
+#        self.file_type = StringVar(top)
+#        self.file_type.set(".png")
+#        self.option = OptionMenu(top, self.file_type, ".png", ".jpeg", ".bmp")
+#        self.option.grid(row = 1, column = 3, padx = 10, pady = 5)
         
-        self.name = None
+#        self.name = None
 
-    def ok(self):
-        self.name = self.e.get() 
-        self.type = self.file_type.get()
-        self.top.destroy()
+#    def ok(self):
+#        self.name = self.e.get() 
+#        self.type = self.file_type.get()
+#        self.top.destroy()
 
 root = Tk()
 root.geometry("900x640+0+0")
